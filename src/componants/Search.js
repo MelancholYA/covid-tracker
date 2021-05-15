f7422d10cc22f89981e322477db7d12020b5e693
melancholYA <ouardi.yacin3@.com>
2021-05-16 00:33:36 +0200

    fixed the search and the date inputs responsivness

--- src/componants/Search.js
+++ src/componants/Search.js
@@ -16,16 +16,13 @@ const Search = ({ disable }) => {
   }, []);
   const searchCountry = (e) => {
     // changing the country names into lower case so the search would be functional
-    const input = e.target.value.toLowerCase();
-    const arr = countries.map((country) => country.Country.toLowerCase());
-
+    const input = e.toLowerCase();
+    const arr = countries.map(function (country) {
+      return { ...country, Country: country.Country.toLowerCase() };
+    });
     if (input.length !== 0) {
-      const newArr = arr.map((country) => {
-        if (country.Country.match(input)) {
-          return country;
-        }
-        return null;
-      });
+      const newArr = arr.filter((country) => country.Country.match(input));
+
       setToShow(newArr);
     }
   };
@@ -41,6 +38,7 @@ const Search = ({ disable }) => {
     dispatch(EDIT(Linkstate));
     disable(false);
   };
+  console.log(toShow);
   return (
     <div className='countries'>
       <header className='countries__header'>
@@ -49,7 +47,7 @@ const Search = ({ disable }) => {
             <path d='M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z' />
           </svg>
           <input
-            onChange={(e) => searchCountry(e)}
+            onChange={(e) => searchCountry(e.target.value)}
             type='text'
             placeholder='start typing your country name...'
           />
@@ -60,7 +58,7 @@ const Search = ({ disable }) => {
         <ul>
           {toShow ? (
             toShow
-              .filter((country) => country.Country !== null)
+              .filter((country) => country.Country)
               .map((country, i) => (
                 <li
                   key={i}
